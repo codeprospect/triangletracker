acced5dfcaf87b0bd2c47c8ad3e6246e9bfef927
waikau maina <mainaeric2099@gmail.com>
2019-02-15 15:42:35 +0000

    styled the page

--- triangle.js
+++ triangle.js
@@ -1,12 +1,17 @@
 function triangleTracker() {
-  var sideOne = document.getElementById('sideOne').value;
-  var sideTwo = document.getElementById('sideTwo').value;
-  var sideThree = document.getElementById('sideThree').value;
-  if (sideOne === sideTwo && sideOne === sideThree) {
+  var sideOne = parseInt(document.getElementById('sideOne').value);
+  var sideTwo = parseInt(document.getElementById('sideTwo').value);
+  var sideThree = parseInt(document.getElementById('sideThree').value);
+
+  var side = [sideOne, sideTwo, sideThree];
+
+  if (side[0] + side[1] <= side[2] || side[0] + side[2] <= side[1] || side[2] + side[1] <= side[0]) {
+    alert("this cannot form a triangle");
+  }else if (side[0] === side[1] && side[0] === side[2]) {
     alert("equilateral triangle");
-  } else if (sideOne === sideTwo || sideOne === sideThree || sideTwo === sideThree) {
+  } else if (side[0] === side[1] || side[0] === side[2] || side[1] === side[2]) {
     alert("Isosceles triangle")
-  } else if (sideOne + sideTwo > sideThree && sideOne + sideThree > sideTwo && sideTwo + sideOne > sideOne) {
+  } else if (side[0] + side[1] > side[2] && side[0] + side[2] > side[1] && side[1] + side[2] > side[0]) {
     alert("Scalene triangle")
   } else {
     alert("Not a triangle");

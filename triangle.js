dc5dcdd0fd86e25ccde397b25022d921233d246f
waikau maina <mainaeric2099@gmail.com>
2019-02-15 08:29:18 +0000

    removed reset button

--- triangle.js
+++ triangle.js
@@ -2,10 +2,13 @@ function triangleTracker() {
   var sideOne = document.getElementById('sideOne').value;
   var sideTwo = document.getElementById('sideTwo').value;
   var sideThree = document.getElementById('sideThree').value;
-  var equip = sideOne + sideTwo + sideThree;
-  if (sideOne = sideTwo && sideOne = sideThree) {
+  if (sideOne === sideTwo && sideOne === sideThree) {
     alert("equilateral triangle");
+  } else if (sideOne === sideTwo || sideOne === sideThree || sideTwo === sideThree) {
+    alert("Isosceles triangle")
+  } else if (sideOne + sideTwo > sideThree && sideOne + sideThree > sideTwo && sideTwo + sideOne > sideOne) {
+    alert("Scalene triangle")
   } else {
-    alert("not a triangle");
+    alert("Not a triangle");
   }
 }
